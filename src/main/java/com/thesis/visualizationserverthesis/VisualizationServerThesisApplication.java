4274ffcd2fb034998c0676f6f0bc150202e7567a
gustavo.alzamora <gustavo.alzamora@pucp.edu.pe>
2021-05-02 13:05:00 -0500

    base project

---
+++ src/main/java/com/thesis/visualizationserverthesis/VisualizationServerThesisApplication.java
@@ -0,0 +1,13 @@
+package com.thesis.visualizationserverthesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualizationServerThesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualizationServerThesisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thesis/visualizationserverthesis/VisualizationServerThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesis.visualizationserverthesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualizationServerThesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
